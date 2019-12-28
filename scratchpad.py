a5c5d348473bc2c75ea156feacca26042dad6397
Ryder Cobean <ryderdavid@gmail.com>
2019-12-28 13:05:36 -0500

    scratch_merged_hist_ver

---
+++ scratchpad.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 28 12:23:01 2019
+
+@author: ryder
+"""
+
+import os
+
+os.chdir(r'C:\Users\ryder\Projects\pynab_expenses')
+
+import sheet_processing_functions as spf
+
+#%%
